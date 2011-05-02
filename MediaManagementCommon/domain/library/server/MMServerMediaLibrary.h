3f27044521bd9b7a04492fae4c83277b342f6e30
Olivier Larivain <olarivain@gmail.com>
2011-05-01 20:22:21 -0700

    - moved server library from cliserver to mm common

---
+++ MediaManagementCommon/domain/library/server/MMServerMediaLibrary.h
@@ -0,0 +1,17 @@
+//
+//  MMiTunesMediaLibrary.h
+//  CLIServer
+//
+//  Created by Kra on 5/1/11.
+//  Copyright 2011 kra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MMMediaLibraryProtected.h"
+
+@interface MMServerMediaLibrary : MMMediaLibrary {
+@private
+    
+}
+
+@end
