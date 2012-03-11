25c0f3381a5bf454e73f1fc4f9e66bd2770044c4
kra Larivain <olarivain@gmail.com>
2012-03-10 16:23:41 -0800

    - added NSDictionary and NSArray safe categories to PCH and started using them

--- MediaManagementCommon/assembler/content/MMContentAssembler.m
+++ MediaManagementCommon/assembler/content/MMContentAssembler.m
@@ -20,7 +20,6 @@
 static MMContentAssembler *sharedInstance;
 
 @interface MMContentAssembler()
-- (void) setInDictionary: (NSMutableDictionary*) dictionary object: (id) object forKey: (id) key;
 - (MMPlaylist*) mediaLibraryForKind: (MMContentKind) kind andSize: (NSUInteger) count;
 @end
 
@@ -44,18 +43,6 @@ static MMContentAssembler *sharedInstance;
   return self;
 }
 
-
-#pragma mark - Convenience methods
-- (void) setInDictionary: (NSMutableDictionary*) dictionary object: (id) object forKey: (id) key
-{
-  if(object == nil)
-  {
-    return;
-  }
-  
-  [dictionary setObject:object forKey: key];
-}
-
 #pragma mark - Domain -> DTO methods
 - (NSDictionary*) writeContent: (MMContent*) content
 {
@@ -63,20 +50,21 @@ static MMContentAssembler *sharedInstance;
   
   
   NSNumber *kind = [NSNumber numberWithInt:content.kind];
-  [self setInDictionary: dto object:kind forKey:@"kind"];
+  [dto setObjectNilSafe: kind forKey:@"kind"];
   
-  [self setInDictionary: dto object:content.contentId forKey:@"contentId"];
+  [dto setObjectNilSafe: content.contentId forKey:@"contentId"];
 
-  [self setInDictionary: dto object:content.name forKey:@"name"];
-  [self setInDictionary: dto object:content.genre forKey:@"genre"];
-  [self setInDictionary: dto object:content.artist forKey:@"artist"];
-  [self setInDictionary: dto object:content.album forKey:@"album"];
-  [self setInDictionary: dto object:content.trackNumber forKey:@"trackNumber"];
-  [self setInDictionary: dto object:content.description forKey:@"description"];
-  [self setInDictionary: dto object:content.show forKey:@"show"];
-  [self setInDictionary: dto object:content.season forKey:@"season"];
-  [self setInDictionary: dto object:content.episodeNumber forKey:@"episodeNumber"];
-  [self setInDictionary: dto object: content.playlistId forKey:@"playlistId"];
+  [dto setObjectNilSafe: content.name forKey:@"name"];
+  [dto setObjectNilSafe: content.genre forKey:@"genre"];
+  [dto setObjectNilSafe: content.artist forKey:@"artist"];
+  [dto setObjectNilSafe: content.album forKey:@"album"];
+  [dto setObjectNilSafe: content.trackNumber forKey:@"trackNumber"];
+  [dto setObjectNilSafe: content.description forKey:@"description"];
+  [dto setObjectNilSafe: content.show forKey:@"show"];
+  [dto setObjectNilSafe: content.season forKey:@"season"];
+  [dto setObjectNilSafe: content.episodeNumber forKey:@"episodeNumber"];
+  [dto setObjectNilSafe: content.playlistId forKey:@"playlistId"];
+  [dto setObjectNilSafe: content.duration forKey: @"duration"];
   
   return dto;
 }
@@ -112,17 +100,17 @@ static MMContentAssembler *sharedInstance;
 - (NSDictionary*) writePlaylist: (MMPlaylist*) library
 {
   NSMutableDictionary *dto = [NSMutableDictionary dictionaryWithCapacity:5];
-  [self setInDictionary: dto object:library.name forKey:@"name"];  
+  [dto setObjectNilSafe:library.name forKey:@"name"];  
   
   NSNumber *kind = [NSNumber numberWithInt:library.kind];
-  [self setInDictionary: dto object:kind forKey:@"kind"];  
+  [dto setObjectNilSafe:kind forKey:@"kind"];  
   
   NSString *uniqueID = library.uniqueId;
-  [self setInDictionary: dto object:uniqueID forKey:@"uniqueId"];  
+  [dto setObjectNilSafe:uniqueID forKey:@"uniqueId"];  
 
   MMContentList *defaultContentList = [library defaultContentList];
   NSArray *content = [self writeContentArray: defaultContentList.content];
-  [self setInDictionary: dto object:content forKey:@"content"];
+  [dto setObjectNilSafe:content forKey:@"content"];
 
   return dto;
 }
@@ -155,18 +143,18 @@ static MMContentAssembler *sharedInstance;
 
 - (MMPlaylist*) createPlaylist:(NSDictionary *)dictionary
 {
-  NSNumber *kindNumber = [dictionary objectForKey:@"kind"];
+  NSNumber *kindNumber = [dictionary nullSafeForKey:@"kind"];
   if(kindNumber == nil)
   {
     return nil;
   }
   
   MMContentKind kind = [kindNumber intValue];
-  NSArray *contents = [dictionary objectForKey: @"content"];
+  NSArray *contents = [dictionary nullSafeForKey: @"content"];
   
   MMPlaylist *playlist = [self mediaLibraryForKind: kind andSize: [contents count]];
-  playlist.uniqueId = [dictionary objectForKey:@"uniqueId"];
-  playlist.name = [dictionary objectForKey:@"name"];
+  playlist.uniqueId = [dictionary nullSafeForKey:@"uniqueId"];
+  playlist.name = [dictionary nullSafeForKey:@"name"];
   
   for(NSDictionary *contentDictionary in contents)
   {
@@ -179,7 +167,7 @@ static MMContentAssembler *sharedInstance;
 
 - (MMContent*) createContent: (NSDictionary*) dictionary
 {
-  NSNumber *kindNumber = [dictionary objectForKey:@"kind"];
+  NSNumber *kindNumber = [dictionary nullSafeForKey:@"kind"];
   if(kindNumber == nil)
   {
     return nil;
@@ -187,17 +175,18 @@ static MMContentAssembler *sharedInstance;
   
   MMContentKind kind = [kindNumber intValue];
   MMContent *content = [MMContent content:kind];
-  content.contentId = [dictionary objectForKey: @"contentId"];
-  content.name = [dictionary objectForKey: @"name"];
-  content.description = [dictionary objectForKey: @"description"];
-  content.genre = [dictionary objectForKey: @"genre"];
-  content.album = [dictionary objectForKey: @"album"];
-  content.artist = [dictionary objectForKey: @"artist"];
-  content.trackNumber = [dictionary objectForKey: @"trackNumber"];
-  content.show = [dictionary objectForKey: @"show"];
-  content.episodeNumber = [dictionary objectForKey: @"episodeNumber"];
-  content.season = [dictionary objectForKey:@"season"];
-  content.playlistId = [dictionary objectForKey: @"playlistId"];
+  content.contentId = [dictionary nullSafeForKey: @"contentId"];
+  content.name = [dictionary nullSafeForKey: @"name"];
+  content.description = [dictionary nullSafeForKey: @"description"];
+  content.genre = [dictionary nullSafeForKey: @"genre"];
+  content.album = [dictionary nullSafeForKey: @"album"];
+  content.artist = [dictionary nullSafeForKey: @"artist"];
+  content.trackNumber = [dictionary nullSafeForKey: @"trackNumber"];
+  content.show = [dictionary nullSafeForKey: @"show"];
+  content.episodeNumber = [dictionary nullSafeForKey: @"episodeNumber"];
+  content.season = [dictionary nullSafeForKey:@"season"];
+  content.playlistId = [dictionary nullSafeForKey: @"playlistId"];
+  content.duration = [dictionary nullSafeForKey: @"duration"];
   return content;
 }
 
